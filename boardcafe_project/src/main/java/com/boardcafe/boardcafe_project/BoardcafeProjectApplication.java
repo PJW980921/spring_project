7cc230f246eac1a24e100c51fc8693baee5fedd7
jw.park <jackgg12322@gmail.com>
2025-03-06 13:35:16 +0900

    setting:"spring boot project setup"

---
+++ boardcafe_project/src/main/java/com/boardcafe/boardcafe_project/BoardcafeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.boardcafe.boardcafe_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardcafeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardcafeProjectApplication.class, args);
+	}
+
+}
---
+++ boardcafe_project/src/test/java/com/boardcafe/boardcafe_project/BoardcafeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boardcafe.boardcafe_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardcafeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
